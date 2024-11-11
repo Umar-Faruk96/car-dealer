ebfa8831ddfaf1dc37c3b79767d2dc3ddd91039f
Umar-Faruq96 <omorfarukjabbar@gmail.com>
2024-11-11 16:58:29 +0600

    login and signup under guest layout

--- resources/views/auth/signup/create.blade.php
+++ resources/views/auth/signup/create.blade.php
@@ -1,51 +1,39 @@
 <x-base-layout pageTitle="Signup" bodyClass="page-signup">
-    <main>
-        <div class="container-small page-login">
-            <div class="flex" style="gap: 5rem">
-                <div class="auth-page-form">
-                    <div class="text-center">
-                        <a href="/">
-                            <img src="/img/logoipsum-265.svg" alt="" />
-                        </a>
-                    </div>
-                    <h1 class="auth-page-title">Signup</h1>
+    <x-guest-layout>
+        <x-slot:heading>
+            <h1 class="auth-page-title">Signup</h1>
+        </x-slot:heading>
 
-                    <form action="" method="post">
-                        <div class="form-group">
-                            <input type="email" placeholder="Your Email" />
-                        </div>
-                        <div class="form-group">
-                            <input type="password" placeholder="Your Password" />
-                        </div>
-                        <div class="form-group">
-                            <input type="password" placeholder="Repeat Password" />
-                        </div>
-                        <hr />
-                        <div class="form-group">
-                            <input type="text" placeholder="First Name" />
-                        </div>
-                        <div class="form-group">
-                            <input type="text" placeholder="Last Name" />
-                        </div>
-                        <div class="form-group">
-                            <input type="text" placeholder="Phone" />
-                        </div>
-                        <button class="btn btn-primary btn-login w-full">Register</button>
-
-                        <div class="grid grid-cols-2 gap-1 social-auth-buttons">
-                            <x-google-button />
-                            <x-facebook-button />
-                        </div>
-                        <div class="login-text-dont-have-account">
-                            Already have an account? -
-                            <a href="/login.html"> Click here to login </a>
-                        </div>
-                    </form>
-                </div>
-                <div class="auth-page-image">
-                    <img src="/img/car-png-39071.png" alt="" class="img-responsive" />
-                </div>
+        <form action="" method="post">
+            <div class="form-group">
+                <input type="email" placeholder="Your Email" />
             </div>
-        </div>
-    </main>
+            <div class="form-group">
+                <input type="password" placeholder="Your Password" />
+            </div>
+            <div class="form-group">
+                <input type="password" placeholder="Repeat Password" />
+            </div>
+            <hr />
+            <div class="form-group">
+                <input type="text" placeholder="First Name" />
+            </div>
+            <div class="form-group">
+                <input type="text" placeholder="Last Name" />
+            </div>
+            <div class="form-group">
+                <input type="text" placeholder="Phone" />
+            </div>
+            <button class="btn btn-primary btn-login w-full">Register</button>
+
+            <div class="grid grid-cols-2 gap-1 social-auth-buttons">
+                <x-google-button />
+                <x-facebook-button />
+            </div>
+            <div class="login-text-dont-have-account">
+                Already have an account? -
+                <a href="/login.html"> Click here to login </a>
+            </div>
+        </form>
+    </x-guest-layout>
 </x-base-layout>
