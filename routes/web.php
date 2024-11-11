3a14124e57045fbe959931cc857fe979c0accf3f
Umar-Faruq96 <omorfarukjabbar@gmail.com>
2024-11-11 18:46:57 +0600

    car resource routes define with car search route

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
-use App\Http\Controllers\{HomeController, SignupController, LoginController};
+use App\Http\Controllers\{CarController, HomeController, LoginController, SignupController};
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/signup', [SignupController::class, 'create'])->name('signup.create');
 Route::get('/login', [LoginController::class, 'create'])->name('login.create');
+
+Route::get('car/search', [CarController::class, 'search'])->name('car.search');
+Route::resource('car', CarController::class);
