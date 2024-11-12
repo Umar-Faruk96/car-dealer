c158b9f954e1db7d45316c4e06f6fcea477f9fab
Umar-Faruq96 <omorfarukjabbar@gmail.com>
2024-11-12 10:44:30 +0600

    update migrations file with column definition

--- database/migrations/2024_11_12_043230_create_models_table.php
+++ database/migrations/2024_11_12_043230_create_models_table.php
@@ -4,24 +4,24 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('models', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+return new class extends Migration {
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void
+	{
+		Schema::create('models', function (Blueprint $table) {
+			$table->id();
+			$table->foreignId('maker_id')->constrained('makers');
+			$table->string('name', 45);
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('models');
-    }
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void
+	{
+		Schema::dropIfExists('models');
+	}
 };
