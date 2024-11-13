d468083ff8eda14e240010ddda536772e183b91d
Umar-Faruq96 <omorfarukjabbar@gmail.com>
2024-11-13 19:03:21 +0600

    data insertion practice by BelongsTo relational factory

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,8 +10,13 @@ class HomeController
 {
 	public function index() : View
 	{
-		Maker::factory()->count(2)
-			->has(Model::factory()->count(2))->create();
+		// Model::factory()->count(2)
+		// 	->forMaker()->create();
+		
+		$maker = Maker::factory()->create();
+		Model::factory()->count(2)
+			// ->for(Maker::factory()->state(['name' => 'Honda']))->create();
+			->for($maker)->create();
 		
 		return view('home.index');
 	}
