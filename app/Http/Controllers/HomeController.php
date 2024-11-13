334684893d141b75bfb9f0b7959e9bd8fa896216
Umar-Faruq96 <omorfarukjabbar@gmail.com>
2024-11-13 14:32:47 +0600

    factory sequence method execute and practice

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,18 +3,21 @@
 namespace App\Http\Controllers;
 
 
-use App\Models\{User};
+use App\Models\{Maker};
 use Illuminate\Contracts\View\View;
 
 class HomeController
 {
 	public function index() : View
 	{
-		// $maker = Maker::factory()->create(['name' => 'Audi']);
-		// dump($maker);
+		$makers = Maker::factory()
+			->count(7)
+			->sequence(['name' => 'Lexus'], ['name' => 'BMW'], ['name' => 'Mercedes'], ['name' => 'Tesla'], ['name' => 'Chevrolet'], ['name' => 'Honda'], ['name' => 'Volkswagen'])
+			->create();
+		dump($makers);
 		
-		$users = User::factory()->count(9)->create();
-		dump($users);
+		// $users = User::factory()->count(9)->create();
+		// dump($users );
 		
 		return view('home.index');
 	}
