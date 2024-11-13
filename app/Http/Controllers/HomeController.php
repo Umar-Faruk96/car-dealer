0707ebed312f803aec94a18c88e3648d84ad9eca
Umar-Faruq96 <omorfarukjabbar@gmail.com>
2024-11-13 14:14:33 +0600

    data insertion practice in the database using factory

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers;
 
 
+use App\Models\{User};
 use Illuminate\Contracts\View\View;
 
 class HomeController
 {
 	public function index() : View
 	{
+		// $maker = Maker::factory()->create(['name' => 'Audi']);
+		// dump($maker);
+		
+		$users = User::factory()->count(9)->create();
+		dump($users);
+		
 		return view('home.index');
 	}
 }
