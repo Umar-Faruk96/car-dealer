5fd2eddc6044da4d0498267d5acdbf38f06d17dd
Umar-Faruq96 <omorfarukjabbar@gmail.com>
2024-11-13 22:42:04 +0600

    db seeder class modify with desired name and email, create user seeder

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,18 +3,12 @@
 namespace App\Http\Controllers;
 
 
-use App\Models\Car;
-use App\Models\User;
 use Illuminate\Contracts\View\View;
 
 class HomeController
 {
 	public function index() : View
 	{
-		// Maker::factory()->count(10)->hasModels(2)->create();
-		
-		User::factory()->has(Car::factory()->count(3), 'favoriteCars')->create();
-		
 		return view('home.index');
 	}
 }
