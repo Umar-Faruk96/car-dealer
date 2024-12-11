3692e1fcb90c7ed8952997452d46ad6b4f3dc08a
Umar-Faruq96 <omorfarukjabbar@gmail.com>
2024-12-11 14:17:50 +0600

    maker and model class component, state and city inline component add; pass real values as props from the controller to view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 
 use App\Models\Car;
+use App\Models\City;
+use App\Models\Maker;
+use App\Models\Model;
+use App\Models\State;
 use Illuminate\Contracts\View\View;
 
 class HomeController
@@ -13,8 +17,12 @@ class HomeController
 		$cars = Car::with(['primaryImage', 'maker', 'model', 'carType', 'fuelType', 'city'])
 			->where('published_at', '<', now())
 			->paginate(15);
+		$makers = Maker::all()->sortBy('name');
+		$models = Model::all()->sortBy('name');
+		$states = State::all()->sortBy('name');
+		$cities = City::all()->sortBy('name');
 		
 		return view('home.index', [
-			'cars' => $cars]);
+			'cars' => $cars, 'makers' => $makers, 'models' => $models, 'states' => $states, 'cities' => $cities]);
 	}
 }
---
+++ resources/views/cars/maker.blade.php
@@ -0,0 +1,3 @@
+@props(['value' => ''])
+
+<option value="{{ $value }}">{{ $slot }}</option>
